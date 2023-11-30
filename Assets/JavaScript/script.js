01da8073c0a405cb34f74d3818454d474443837b
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2023-11-29 20:23:20 -0500

    lays out basic skeleton script

---
+++ Assets/JavaScript/script.js
@@ -0,0 +1,42 @@
+// DEPENDENCIES
+
+// start button
+var startBtn = document.querySelector('#startBtn');
+//view high-score button
+var viewScrBtn = document.querySelector('#viewHighScrBtn');
+// timer display
+var timerDisplay = document.querySelector('#time');
+//submit high-score button
+
+//reset high-score button
+
+
+// DATA
+
+// list of questions
+
+// list of answers
+
+// timer
+
+// score
+
+// high-scores 
+
+
+// FUNCTIONS
+
+function displayQuestion () {
+
+}
+
+function startGame () {
+
+}
+
+function startTimer () {
+
+}
+
+
+// USER INTERACTIONS
