9a2927747fc157bb91c529202692e64b72d6f17d
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2023-11-29 20:56:03 -0500

    creates questions

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -11,9 +11,35 @@ var timerDisplay = document.querySelector('#time');
 //reset high-score button
 
 
+
 // DATA
 
 // list of questions
+var questions = [{
+    q: "Where is the JavaScript page linked?",
+    a: [{ text: "In the 'head' element", isCorrect: false },
+    { text: "At the bottom of the 'body' element", isCorrect: true },
+    { text: "In the 'CSS' file", isCorrect: false },
+    { text: "On Kanye's twitter page", isCorrect: false }
+    ]
+},
+{
+    q: "What is JavaScript used for?",
+    a: [{ text: "To give the webpage interactive functionality", isCorrect: true },
+    { text: "To style the aesthetics of the webpage", isCorrect: false },
+    { text: "To update you TikTok algorithm", isCorrect: false },
+    { text: "To manage cross-server databases", isCorrect: false }
+    ]
+},
+{
+    q: "How do you call a function named 'someFunction?",
+    a: [{ text: "call function someFunction<>", isCorrect: false },
+    { text: "call someFunction[]", isCorrect: false },
+    { text: "What are you, 73? just text it", isCorrect: false },
+    { text: "someFunction()", isCorrect: true }
+    ]
+},
+];
 
 // list of answers
 
@@ -22,6 +48,7 @@ var timerDisplay = document.querySelector('#time');
 // score
 
 // high-scores 
+
 
 
 // FUNCTIONS
@@ -39,4 +66,20 @@ function startTimer () {
 }
 
 
+
 // USER INTERACTIONS
+
+// user presses start button
+startBtn.addEventListener("click", function() {
+    console.log("start button works")
+});
+
+// user presses view high-score button
+viewScrBtn.addEventListener("click", function() {
+    console.log("view score button works")
+});
+
+// user presses submit high-score button
+
+
+// user presses reset high-score button
